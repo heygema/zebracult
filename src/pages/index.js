7da5e97e5a8ee8d33d8eb85f8dd2474e6c0fb997
Gema <gem.anggada@gmail.com>
2020-12-19 03:11:45 +0700

    finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,73 @@
-import { useEffect, useRef, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import Head from "next/head";
-import { Canvas } from "react-three-fiber";
+import { Canvas, useFrame } from "react-three-fiber";
 import { useRouter } from "next/router";
 import { Html, useGLTF, OrbitControls, Stars, useProgress } from "drei";
+import { animated, useTransition } from "react-spring";
 
 const superSlow = 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001;
 
+const YOUTUBE_URL = "https://www.youtube.com/watch?v=";
+
+const youtubeIds = [
+  "5ESS_mAHHYU",
+  "U2MCBAnpF2A",
+  "rlNUZoy0Pgg",
+  "WgqOczKdTjQ",
+  "j1qzTlz2Moc",
+  "xnfjQUJ83LY",
+  "MOX3BQSW7_c"
+];
+
+const lastIndex = youtubeIds.length - 1;
+
 function Model({ url }) {
   const gltf = useGLTF(url, true);
-  return;
+  return <primitive object={gltf.scene} dispose={null} />;
 }
 
-function Main() {
+function Main({ router }) {
   const [inputState, setInputState] = useState("");
+  const [showText, setShowText] = useState(false);
   const [worthy, setWorthy] = useState(false);
 
+  const [x, setX] = useState(0);
+
+  useFrame(() => {
+    setX(x - 0.003);
+  });
+
+  const transitions = useTransition(showText, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
+  const worthyTransition = useTransition(worthy, null, {
+    from: { position: "absolute", opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
   const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (!showText) {
+      setShowText(true);
+    }
+  }, []);
 
   useEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus();
     }
-  });
+  }, []);
 
   useEffect(() => {
+    const randomIndex = Math.round(Math.random() * lastIndex);
     if (inputState.toLowerCase().includes("you only live once")) {
       setWorthy(true);
+      router.push(YOUTUBE_URL + youtubeIds[randomIndex]);
     } else {
       setWorthy(false);
     }
@@ -36,22 +78,32 @@ function Main() {
   const year = currentYear === 2020 ? currentYear : `2020-${currentYear}`;
 
   return (
-    <Canvas>
+    <>
       <OrbitControls
-        enableDamping
-        dampingFactor={0.0003}
-        rotateSpeed={superSlow}
         autoRotate
+        enableDamping
+        dampingFactor={0.25}
+        rotateSpeed={superSlow}
         enableRotate={false}
         enableZoom={false}
       />
+      <Suspense fallback={"Loading..."}>
+        <mesh rotation={[-0.2, x, 0]} position={[0, 1.4, 0]}>
+          <Model url="/zebra/scene.gltf" />
+        </mesh>
+      </Suspense>
 
       <Html fullscreen>
         <div className="container">
           <div className="phrase-container">
-            <h2 className="phrase">
-              There's one Zebra, with stripes like a Xylophone.
-            </h2>
+            {transitions.map(
+              ({ item, key, props }) =>
+                item && (
+                  <animated.h2 key={key} props={props} className="phrase">
+                    There's one Zebra, with stripes like a Xylophone.
+                  </animated.h2>
+                )
+            )}
             <input
               ref={inputRef}
               type="text"
@@ -61,8 +113,16 @@ function Main() {
                 setInputState(e.target.value);
               }}
             />
-            {worthy ? <h3>you are worthy. ☑️ </h3> : <h3>&nbsp;</h3>}
+            {worthyTransition.map(
+              ({ item, key, props }) =>
+                item && (
+                  <animated.h3 key={key} props={props}>
+                    you are worthy. ☑️{" "}
+                  </animated.h3>
+                )
+            )}
           </div>
+
           <footer>
             <span>{`© ${year} #zebracult.`}</span>
             <a href="https://sketchfab.com/crazymanuel" target="blank">
@@ -72,7 +132,7 @@ function Main() {
         </div>
       </Html>
       <Stars />
-    </Canvas>
+    </>
   );
 }
 
@@ -85,7 +145,10 @@ function Home() {
         <title>Zebra Cult</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main />
+
+      <Canvas>
+        <Main router={router} />
+      </Canvas>
     </>
   );
 }
