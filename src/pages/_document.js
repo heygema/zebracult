e1a3a58d8823c83918aca17edebc4360467bd73d
Gema <gem.anggada@gmail.com>
2020-12-25 20:22:18 +0700

    removed analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,18 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-const yandexMetrica = `
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(70655017, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
-
-`;
-
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -23,12 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: yandexMetrica
-            }}
-          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap"
